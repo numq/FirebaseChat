28c9948b5bbc83c87d408313b543b1b5ea86d612
numq <numq.dev@gmail.com>
2022-08-03 17:07:32 +0300

    Add interactor

---
+++ app/src/main/java/com/numq/firebasechat/interactor/UseCase.kt
@@ -0,0 +1,24 @@
+package com.numq.firebasechat.interactor
+
+import android.util.Log
+import arrow.core.Either
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+abstract class UseCase<in T, R> {
+
+    private val coroutineContext = Dispatchers.Main + Job()
+    private val coroutineScope = CoroutineScope(coroutineContext)
+
+    abstract fun execute(arg: T): Either<Exception, R>
+
+    operator fun invoke(arg: T, onResult: (Either<Exception, R>) -> Unit) {
+        coroutineScope.launch {
+            onResult(execute(arg).tap {
+                Log.d(javaClass.simpleName, it.toString())
+            })
+        }
+    }
+}
