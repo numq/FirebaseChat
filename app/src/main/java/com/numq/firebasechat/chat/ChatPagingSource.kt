ac651a3f9ce0213c04bc7db9808c84ee379f3af9
numq <numq.dev@gmail.com>
2022-08-17 18:01:18 +0300

    Remove paging

--- app/src/main/java/com/numq/firebasechat/chat/ChatPagingSource.kt
+++
@@ -1,6 +0,0 @@
-package com.numq.firebasechat.chat
-
-import com.numq.firebasechat.paging.LimitPagingSource
-
-class ChatPagingSource(limit: Int, loadMore: (Int, Int) -> List<Chat>) :
-    LimitPagingSource<Chat>(limit, loadMore)
--- app/src/main/java/com/numq/firebasechat/message/MessagePagingSource.kt
+++
@@ -1,6 +0,0 @@
-package com.numq.firebasechat.message
-
-import com.numq.firebasechat.paging.LimitPagingSource
-
-class MessagePagingSource(limit: Int, loadMore: (Int, Int) -> List<Message>) :
-    LimitPagingSource<Message>(limit, loadMore)
