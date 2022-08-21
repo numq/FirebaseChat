1f26f3a5144c3803644bff8b8a8a1b212162ddfd
numq <numq.dev@gmail.com>
2022-08-21 16:06:35 +0300

    Change input validation constraints

--- app/src/main/java/com/numq/firebasechat/auth/InputValidator.kt
+++ app/src/main/java/com/numq/firebasechat/auth/InputValidator.kt
@@ -1,9 +1,19 @@
 package com.numq.firebasechat.auth
 
 object InputValidator {
-    val validateString: (String) -> String =
-        { input -> input.filter { it in arrayOf('.', '@') || it.isLetterOrDigit() } }
-    val emailPattern = Regex("^[\\w\\-.]+@[\\w.]+\\.[\\w]+\$")
+
+    private val emailPattern = Regex("^[\\w\\-.]+@[\\w.]+\\.[\\w]+\$")
+    val nameConstraints = 3..16
     val emailConstraints = 3..32
     val passwordConstraints = 8..32
+
+    fun validateString(input: String) =
+        input.filter { it in arrayOf('.', '@') || it.isLetterOrDigit() }
+
+    fun validateName(name: String) = name.length in nameConstraints
+    fun validateEmail(email: String) =
+        email.length in emailConstraints && emailPattern.matches(email)
+
+    fun validatePassword(password: String) = password.length in passwordConstraints
+
 }
