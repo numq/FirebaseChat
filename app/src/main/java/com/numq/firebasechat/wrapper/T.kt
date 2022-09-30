e47cf6053a4d44e610958f6d73c61d1993bb8bdd
numq <numq.dev@gmail.com>
2022-09-30 13:46:26 +0300

    Provide wrapper changes

--- app/src/main/java/com/numq/firebasechat/wrapper/T.kt
+++ app/src/main/java/com/numq/firebasechat/wrapper/T.kt
@@ -4,15 +4,12 @@ import arrow.core.Either
 import arrow.core.flatten
 import arrow.core.left
 import arrow.core.right
-import com.numq.firebasechat.network.NetworkApi
-import com.numq.firebasechat.network.NetworkException
 
 fun <T> T.wrap(): Either<Exception, T> = runCatching { this }.fold({ it.right() },
-        { Exception(it.localizedMessage).left() })
+    { Exception(it.localizedMessage).left() })
 
-fun <T> T.wrap(networkApi: NetworkApi? = null): Either<Exception, T> = networkApi?.let {
+fun <T> T.wrapIf(condition: Boolean, exception: Exception): Either<Exception, T> =
     Either.conditionally(
-        networkApi.isAvailable,
-        ifFalse = { NetworkException.Default },
+        condition,
+        ifFalse = { exception },
         ifTrue = { wrap() }).flatten()
-} ?: wrap()
--- app/src/main/java/com/numq/firebasechat/wrapper/Task.kt
+++ app/src/main/java/com/numq/firebasechat/wrapper/Task.kt
@@ -5,16 +5,13 @@ import arrow.core.flatten
 import arrow.core.left
 import arrow.core.right
 import com.google.android.gms.tasks.Task
-import com.numq.firebasechat.network.NetworkApi
-import com.numq.firebasechat.network.NetworkException
 import kotlinx.coroutines.tasks.await
 
 suspend fun <T> Task<T>.wrap() = runCatching { await() }.fold({ it.right() },
     { Exception(it.localizedMessage).left() })
 
-suspend fun <T> Task<T>.wrap(networkApi: NetworkApi?) = networkApi?.let {
+suspend fun <T> Task<T>.wrapIf(condition: Boolean, exception: Exception) =
     Either.conditionally(
-        networkApi.isAvailable,
-        ifFalse = { NetworkException.Default },
+        condition,
+        ifFalse = { exception },
         ifTrue = { wrap() }).flatten()
-} ?: wrap()
