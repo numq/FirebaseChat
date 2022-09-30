ab1d4a36b4207c31650ab201ce499537e7c2c624
numq <numq.dev@gmail.com>
2022-09-30 13:53:03 +0300

    Provide state changes

--- app/src/main/java/com/numq/firebasechat/navigation/NavState.kt
+++ app/src/main/java/com/numq/firebasechat/navigation/NavState.kt
@@ -1,10 +1,10 @@
 package com.numq.firebasechat.navigation
 
+import com.numq.firebasechat.auth.AuthenticationState
 import com.numq.firebasechat.network.NetworkStatus
 
 data class NavState(
-    val authenticating: Boolean = true,
-    val userId: String? = null,
     val status: NetworkStatus? = null,
+    val authenticationState: AuthenticationState? = null,
     val exception: Exception? = null
 )
